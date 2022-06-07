def7c1b5560673acabbbe69b7ab03aff5d5acff5
seunghyecho <tmdgp523@gmail.com>
2022-06-08 00:32:08 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <div className="App">
+      <Post />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Post.jsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { useState } from "react";
+import Layout from "./Layout";
+
+export default function Post() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, body);
+    axios.post("http://localhost:3001/posts", {
+      title,
+      body,
+    });
+    setTitle("");
+    setBody("");
+  };
+  return (
+    <Layout>
+      <form>
+        <h1>Create a blog post</h1>
+        <div>
+          <label htmlFor="post-title">Title</label>
+          <input
+            type="text"
+            name="post-title"
+            id="post-title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="post-body"></label>
+          <textarea
+            name="post-body"
+            id="post-body"
+            cols="30"
+            rows="10"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
+        </div>
+        <button type="submit" onClick={onSubmit}>
+          등록
+        </button>
+      </form>
+    </Layout>
+  );
+}
